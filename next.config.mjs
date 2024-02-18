6f15178125b599e6462ff2f68c5fbd0650c02875
waleed <waleed.naseem1@gmail.com>
2024-02-18 10:53:14 +0500

    waleed

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+},
   reactStrictMode: true,
 };
 
